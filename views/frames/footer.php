ef87a25c007045d4874f035e662df1841f34c49d
EdwinjAleman <jeagudelo152@misena.edu.co>
2020-12-03 00:40:53 -0500

    lo que sea

--- views/frames/footer.php
+++ views/frames/footer.php
@@ -4,7 +4,7 @@
 
 
 
-	<script type="text/javascript" src="../assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="../assets/DataTables/jQuery-3.3.1/jquery-3.5.1.min.js"></script>
 	<script type="text/javascript" src="../assets/DataTables/Bootstrap-4-4.1.1/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="../assets/DataTables/datatables.min.js"></script>
 	<script type="text/javascript" src="../assets/js/jquery-confirm.js"></script>
