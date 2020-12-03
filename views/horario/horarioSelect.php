2b7fe0e1d5754fd7881c1ff60162653278d94e72
lewis <lewisv4re@gmail.com>
2020-12-03 16:50:34 -0500

    calendario

--- views/horario/horarioSelect.php
+++ views/horario/horarioSelect.php
@@ -16,6 +16,25 @@
       buttonIcons: false, // show the prev/next text
       weekNumbers: true,
       navLinks: true, // can click day/week names to navigate views
+      selectable: true,
+      selectMirror: true,
+      select: function(arg) {
+        var title = prompt('Event Title:');
+        if (title) {
+          calendar.addEvent({
+            title: title,
+            start: arg.start,
+            end: arg.end,
+            allDay: arg.allDay
+          })
+        }
+        calendar.unselect()
+      },
+      eventClick: function(arg) {
+        if (confirm('Are you sure you want to delete this event?')) {
+          arg.event.remove()
+        }
+      },
       editable: true,
       dayMaxEvents: true, // allow "more" link when too many events
       events: [
