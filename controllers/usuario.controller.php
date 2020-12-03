cb11c5d07704f752c789b2a7f420f8fd751ca975
EdwinjAleman <jeagudelo152@misena.edu.co>
2020-12-03 01:53:54 -0500

    oooo

--- controllers/usuario.controller.php
+++ controllers/usuario.controller.php
@@ -17,7 +17,8 @@
 										if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="1"){ require_once('frames/sidebaradministrador.php'); }
 										if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="2"){ require_once('frames/menuAprendiz.php'); }
 										if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="3"){ require_once('frames/menuInstructor.php'); }
-										    require_once('../views/frames/footer.php');
+										require_once('../views/usuario/usuarioView.php');
+										require_once('../views/frames/footer.php');
 									}
 
 
