47fed3ca22495709ab98c7b0dd22214a5566a068
lewis <lewisv4re@gmail.com>
2020-12-03 11:37:20 -0500

    correccion

--- views/error404.php
+++ views/error404.php
@@ -11,11 +11,10 @@ require_once('../views/frames/footer.php');
         <br>
         <div class="row">
           <div class="col-md-8 col-md-offset-2">
-            <a >suqchaenfour</a>
-            <a > giron es gei</a>
+            
           </div>
         </div>
-        <h5 class="mt">Oye, tal vez te interese esta pagina: puto</h5>
+        <h5 class="mt">Oye, tal vez te interese esta pagina:</h5>
         <p><a href="main.php">Index</a>
       </div>
     </div>
