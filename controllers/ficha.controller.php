e2987fd895c95efd19c8eca9b1df328d7b7bda8f
EdwinjAleman <jeagudelo152@misena.edu.co>
2020-12-07 18:00:06 -0500

    footers

--- controllers/ficha.controller.php
+++ controllers/ficha.controller.php
@@ -16,7 +16,8 @@ class FichaController{
 									if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="1"){ require_once('frames/sidebaradministrador.php'); }
 									if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="2"){ require_once('frames/menuAprendiz.php'); }
 									if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="3"){ require_once('frames/menuInstructor.php'); }
-									require_once('../views/modulos/ficha/fichaView.php');
+                                    require_once('../views/modulos/ficha/fichaView.php');
+                                    require_once('../views/frames/footer.php');
                                 }
                                 
     public function Insertar()
