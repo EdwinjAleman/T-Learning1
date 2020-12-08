ba18628d6ac8544439217450c886f7afc802d18d
EdwinjAleman <jeagudelo152@misena.edu.co>
2020-12-08 16:37:28 -0500

    ..

--- views/modulos/programafor/programaforInsert.php
+++ views/modulos/programafor/programaforInsert.php
@@ -1,75 +1,59 @@
-<!-- <form name="formprogramafor" id="formprogramafor" onSubmit="Crear(); return false;">
+<div class="modal fade" name="formprogramafor" id="formprogramafor" data-backdrop="static" data-keyboard="false"
+    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">CREAR PROGRAMA DE FORMACION: </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
 
-    <h1> CREAR PROGRAMA DE FORMACION: </h1>
-
-    <input type="text" name="pro_for_id" hidden> <br>
-
-    <label for="pro_for_nombr"> Nombre programa de formacion: </label> <br>
-    <input type="text" name="pro_for_nombr"> <br>
-
-    <label for="pro_for_abrvtr"> Abreviatura: </label> <br>
-    <input type="text" name="pro_for_abrvtr"> <br>
-
-    <label for="pro_for_tip_id">Tipos de Programas: </label><br>
-    <select name="pro_for_tip_id">
-        <?php 
-               foreach ( $this->programafor->SelectTipoPro() as $filas ): 
-						echo '<option value="'.$filas->tip_pro_id.'">'.$filas->tip_pro_nombr.'</option>';
-               endforeach; 
-         ?>
-
-    </select><br><br>
-
-    <input type="submit" value="Crear" id="btnguardar">
-
-</form> -->
-
-<section id="main-content">
-    <section class="wrapper">
-        <h3><i class="fa fa-angle-right"></i> Formulario </h3>
-        <div class="row mt">
-            <div class="col-lg-12">
-                <div class="form-panel">
+                <form action="">
                     <h4 class="mb"><i class="fa fa-angle-right"></i> Ingrese los siguientes datos para agregar un
-                        programa de formacion</h4>
-                    <form class="form-horizontal style-form" name="formprogramafor" id="formprogramafor"
-                        onSubmit="Crear(); return false;">
+                        programa de formacion:</h4>
+                    <input type="text" name="pro_for_id" hidden>
 
-                        <input type="text" name="pro_for_id" hidden>
-
-                        <div class="form-group">
-                            <label class="col-sm-2 col-sm-2 control-label" for="pro_for_nombr">
-                                Nombre programa de formacion:
-                            </label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" name="pro_for_nombr">
-                            </div>
+                    <div class="form-group ">
+                        <label for="pro_for_nombr" class="control-label col-lg-4"> Nombre programa de formacion:</label>
+                        <div class="col-lg-10">
+                            <input class=" form-control" type="text" id="pro_for_nombr" name="pro_for_nombr" required />
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            <label class="col-sm-2 col-sm-2 control-label" for="pro_for_abrvtr">
-                                Abreviatura del Programa de Formacion:
-                            </label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" name="pro_for_abrvtr">
-                            </div>
+                    <div class="form-group ">
+                        <label for="pro_for_abrvtr" class="control-label col-lg-4"> Abreviatura del Programa de
+                            Formacion:</label>
+                        <div class="col-lg-10">
+                            <input class=" form-control" type="text" id="pro_for_abrvtr" name="pro_for_abrvtr"
+                                required />
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            <label class="col-sm-2 col-sm-2 control-label">
-                                Tipos de Programas:
-                            </label>
-                            <select class="form-control" name="pro_for_tip_id">
-                                <?php foreach ( $this->programafor->SelectTipoPro() as $filas ): 
+                    <div class="form-group">
+                        <label class="col-sm-2 col-sm-2 control-label">
+                            Tipos de Programas:
+                        </label>
+                        <select class="form-control" name="pro_for_tip_id">
+                            <?php foreach ( $this->programafor->SelectTipoPro() as $filas ): 
                                        echo '<option value="'.$filas->tip_pro_id.'">'.$filas->tip_pro_nombr.'</option>';
                                  endforeach; ?>
-                            </select>
-                        </div>
-                        <input type="submit" value="Crear" id="btnguardar">
-                    </form>
-                </div>
+                        </select>
+                    </div>
+
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button class="btn btn-primary" onclick="Crear();" value="Grabar" id="btnguardar">Crear</button>
+                    </div>
+
+                </form>
+
             </div>
+
         </div>
-    </section>
-</section>
+    </div>
+</div>
+
 
--- views/modulos/programafor/programaforView.php
+++ views/modulos/programafor/programaforView.php
@@ -1,3 +1,11 @@
+<section id="main-content">
+    <section class="wrapper">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formprogramafor">Nueva Jornada</button>
+    </section>
+</section>
+
+
+
 <div id="main">
 
     <script src="../assets/js/modulos/scripts.programafor.js"></script>
