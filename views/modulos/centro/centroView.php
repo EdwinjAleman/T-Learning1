015e40dcbcd110b2f63bdfd84f9c077dc9b0bb10
Mayerlin <luz.mayerlin2549@gmail.com>
2020-12-09 15:08:29 -0500

    hola bebes

--- views/modulos/centro/centroView.php
+++ views/modulos/centro/centroView.php
@@ -1,14 +1,1 @@
- <section id="main-content">
-<div id="main">
-
-    
-    <script src="../assets/js/modulos/scripts.centro.js"></script>
-
-    <div id="tview">
-        <?php  include_once('centroInsert.php'); ?>
-        <?php  include_once('centroSelect.php'); ?>
-    </div>
-
-</div>
-
-</section> 
+<h1>son putos</h1>
