ee4d6be7db8b0244c8262b4de4668ca6fbde5209
Mayerlin <luz.mayerlin2549@gmail.com>
2020-12-09 07:11:52 -0500

    Cambios sin terminar centro

--- models/centro.php
+++ models/centro.php
@@ -11,54 +11,85 @@
 		}
 
 		public function Select(){
-										try 					{	
-																	$sql=$this->pdo->prepare("SELECT * FROM tbl_inf_centr ORDER BY inf_id desc");
-																	$sql->execute();
-																	return $sql->fetchALL(PDO::FETCH_OBJ);
-																}
+			try 					{	
+										$sql=$this->pdo->prepare("SELECT * FROM tbl_inf_centr ORDER BY inf_id desc");
+										$sql->execute();
+										return $sql->fetchALL(PDO::FETCH_OBJ);
+									}
 
-										catch (Exception $e) 	{	
-																	die($e->getMessage());
-																}
+			catch (Exception $e) 	{	
+										die($e->getMessage());
+									}
+		}
+		
+		public function Insert(Centro $data){
+								
+			try 					{	$sql = "INSERT INTO tbl_inf_centr (inf_soms, inf_misn ,inf_visn, inf_dirctr_genrl, inf_dirctr_regnl, inf_subdrctr_centr, inf_cordndr_msnl, inf_ubi, inf_lin, inf_atncn) 
+												VALUES (?,?,?,?,?,?,?,?,?,?)";
+												$this->pdo->prepare($sql) 
+															->execute(
+																array(
+																	$data->inf_soms,
+																	$data->inf_misn,
+																	$data->inf_visn,
+																	$data->inf_dirctr_genrl,
+																	$data->inf_dirctr_regnl,
+																	$data->inf_subdrctr_centr,
+																	$data->inf_cordndr_msnl,
+																	$data->inf_ubi,
+																	$data->inf_lin,
+																	$data->inf_atncn
+
+																	
+
+																)
+															);
+																		
+									}
+
+							    catch (Exception $e) { die($e->getMessage()); }
+		} 
+
+		public function Delete($inf_id){
+								
+			try 					{	$sql="DELETE FROM tbl_inf_centr WHERE inf_id=?";
+										$this->pdo->prepare($sql)
+													  ->execute(
+															  array(
+																		$inf_id
+																  )
+															  );
+									  }
+
+			catch(exception $e)		{ die ($e->getMessage()); 				 }
 		}
 
-		
-		
-		public function Delete($id){
-
+		public function Update(Centro $data){
 								
-										try 					{	$sql="DELETE FROM tbl_anuncio WHERE anu_id=?";
-																	$this->pdo->prepare($sql)
-										  					  				  ->execute(
-										  												array(
-										  					  									$id	
-										  													)
-										  												);
-										  						}
-
-										catch(exception $e)		{ die ($e->getMessage()); 				 }
-}
-
-public function Update(Anuncio $data){
-								
-			try 					{	$sql = "UPDATE tbl_anuncio SET anu_descripcion=?,anu_titulo=?,anu_fechaFin=?,anu_fechaCreacion=?,anu_nombreCreador=?
-															  WHERE anu_id=?";
+			try 					{	$sql = "UPDATE tbl_inf_centr SET inf_soms=?, inf_misn=?, inf_visn=?, inf_dirctr_genrl=?, inf_dirctr_regnl=?,inf_subdrctr_centr=?,inf_cordndr_msnl=?, inf_ubi=?, inf_lin=?, inf_atncn=?
+															  WHERE inf_id=?";
 										   $this->pdo->prepare($sql) 
 													 ->execute(
 																array(
-																	$data->anu_descripcion,
-																	$data->anu_titulo,
-																    $data->anu_fechaFin,
-																	$data->anu_fechaCreacion,
-																	$data->anu_nombreCreador,
-																	$data->aun_id
+																	$data->inf_soms,
+																	$data->inf_misn,
+																	$data->inf_visn,
+																	$data->inf_dirctr_genrl,
+																	$data->inf_dirctr_regnl,
+																	$data->inf_subdrctr_centr,
+																	$data->inf_cordndr_msnl,
+																	$data->inf_ubi,
+																	$data->inf_lin,
+																	$data->inf_atncn,
+																	$data->inf_id
 																)
 															);
 					}
 
-	catch (Exception $e) { die($e->getMessage()); }
+			catch (Exception $e) { die($e->getMessage()); }
 
 } 
 
-	}
+	
+}
 ?>
