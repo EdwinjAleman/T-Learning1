6bdf617e9f3dc8df6d7e985fc5ac20c486d89789
Mayerlin <luz.mayerlin2549@gmail.com>
2020-12-09 07:30:58 -0500

    PERSONA TERMINADO

--- views/modulos/persona/personaView.php
+++ views/modulos/persona/personaView.php
@@ -1,8 +1,12 @@
+
+
 <div id="main">
 
     <script src="../assets/js/modulos/scripts.persona.js"></script>
 
     <div id="tview">
+
+    
         <?php  include_once('personaInsert.php'); ?>
         <?php  include_once('personaSelect.php'); ?>
     </div>
