64be07130c0df14e7f67307150dcdd95a9013509
agoniagiron <andrespipe490@gmail.com>
2020-12-09 06:47:09 -0500

    ...

--- views/modulos/persona/personaView.php
+++ views/modulos/persona/personaView.php
@@ -1,23 +1,10 @@
-<section id="main-content">
-    <section class="wrapper">
+<div id="main">
 
-        <div id="main">
+    <script src="../assets/js/modulos/scripts.persona.js"></script>
 
-<<<<<<< HEAD
     <div id="tview">
     <?php  include_once('personaInsert.php'); ?>
-        <?php  include_once('personaSelect.php'); ?>
-    </div>
-=======
-            <script src="../assets/js/modulos/scripts.persona.js"></script>
->>>>>>> e9513142ae11d5407302411a9c87e5a3c0b02fda
-
-            <div id="tview">
-                <?php  include_once('personaInsert.php'); ?>
                 <?php  include_once('personaSelect.php'); ?>
-            </div>
+    </div>
 
-        </div>
-
-    </section>
-</section>
+</div>
