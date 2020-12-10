376088fc42f46e1b68f03b8d3f7e9edc6eeb0f0d
EdwinjAleman <jeagudelo152@misena.edu.co>
2020-12-10 11:16:56 -0500

    icono

--- views/frames/header.php
+++ views/frames/header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/ico"  href="../assets/img/desktop_icon_174568.ico">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
