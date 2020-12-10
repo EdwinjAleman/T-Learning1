a5b22b9246f5cdb262dd6180dc9714301d85e432
EdwinjAleman <jeagudelo152@misena.edu.co>
2020-12-10 02:36:36 -0500

    ole

--- controllers/datousu.controller.php
+++ controllers/datousu.controller.php
@@ -12,11 +12,12 @@ class DatousuController{
 
     public function Index()
 								{
-                                    //require_once('../views/frames/header.php');
-								    //if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="1"){ require_once('frames/sidebaradministrador.php'); }
-									//if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="2"){ require_once('frames/menuAprendiz.php'); }
-									//if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="3"){ require_once('frames/menuInstructor.php'); }
-									require_once('../views/modulos/datousu/datousuView.php');
+                                    require_once('../views/frames/header.php');
+								    if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="1"){ require_once('frames/sidebaradministrador.php'); }
+									if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="2"){ require_once('frames/menuAprendiz.php'); }
+									if (isset($_SESSION['SRol']) and $_SESSION['SRol']=="3"){ require_once('frames/menuInstructor.php'); }
+                                    require_once('../views/modulos/datousu/datousuView.php');
+                                    require_once('../views/frames/footer.php');
                                 }
                                 
     public function Insertar()
