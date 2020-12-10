eefb8cf998975244c63a2f69f55209c79c817ad9
lewis <lewisv4re@gmail.com>
2020-12-10 13:33:07 -0500

    agenda

--- views/modulos/agenda/agendaSelect.php
+++ views/modulos/agenda/agendaSelect.php
@@ -1,11 +1,21 @@
-<?php 
-foreach ($this->agenda->Select() as $filas): ?>
-<?php $grupal="'".$filas->per_id."','".$filas->per_nombr."','".$filas->per_aplld."','".$filas->per_cedl."','".$filas->per_dirccn."','".$filas->per_corr."','".$filas->per_telfn."','".$filas->per_areid."'" ?>
-
-<?php echo $filas->usu_fot; ?><br>
+<!-- <?php echo $filas->usu_fot; ?><br>
 <?php echo $filas->per_nombr; ?><br>
 <?php echo $filas->per_aplld; ?><br>
 <?php echo $filas->per_corr; ?><br>
 <?php echo $filas->per_telfn; ?><br>
+ -->
+
+<?php foreach ($this->agenda->Select() as $filas): ?>
+<?php $grupal="'".$filas->per_id."','".$filas->per_nombr."','".$filas->per_aplld."','".$filas->per_cedl."','".$filas->per_dirccn."','".$filas->per_corr."','".$filas->per_telfn."','".$filas->per_areid."'" ?>
+
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Agenda De personas </h5>
+    <h6 class="card-subtitle mb-2 text-muted">Informacion</h6>
+    <p class="card-text"><?php echo $filas->per_nombr; ?> <?php echo $filas->per_aplld; ?></p>
+    <a href="#" class="card-link"><?php echo $filas->per_corr; ?><br></a>
+    <a href="#" class="card-link"><?php echo $filas->per_telfn; ?></a>
+  </div>
+</div> 
 
 <?php endforeach; ?>
--- views/modulos/agenda/agendaView.php
+++ views/modulos/agenda/agendaView.php
@@ -1,9 +1,13 @@
-<div id="main">
+<section id="main-content">
+    <section class="wrapper">
+        <div id="main">
 
-    <script src="../assets/js/modulos/scripts.agenda.js"></script>
+            <script src="../assets/js/modulos/scripts.agenda.js"></script>
 
-    <div id="tview">
-        <?php  include_once('agendaSelect.php'); ?>
-    </div>
+            <div id="tview">
+                <?php  include_once('agendaSelect.php'); ?>
+            </div>
 
-</div>
+        </div>
+    </section>
+</section>
