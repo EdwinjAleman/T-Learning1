eabd0a82110dd53fe332075f85c64e4e6b8d814a
lewis <lewisv4re@gmail.com>
2020-12-10 00:16:32 -0500

    corrreccion de los main para que no se sobrepongan en el slide

--- views/modulos/tipopro/tipoproView.php
+++ views/modulos/tipopro/tipoproView.php
@@ -1,15 +1,20 @@
 <section id="main-content">
-        <section class="wrapper">
-        <button type="button" class="btn btn-primary" data-toggle="modal"
-                data-target="#formtipopro">Nuevo Tipo Programa</button>    
+    <section class="wrapper">
+        <section id="main-content">
+            <section class="wrapper">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formtipopro">Nuevo Tipo
+                    Programa</button>
+            </section>
         </section>
+
+        <div id="main">
+
+            <script src="../assets/js/modulos/scripts.tipopro.js"></script>
+            <div id="tview">
+                <?php  include('tipoproInsert.php'); ?>
+                <?php  include('tipoproSelect.php'); ?>
+            </div>
+        </div>
+
     </section>
-
-<div id="main">
-
-    <script src="../assets/js/modulos/scripts.tipopro.js"></script>
-    <div id="tview">
-        <?php  include('tipoproInsert.php'); ?>
-        <?php  include('tipoproSelect.php'); ?>
-    </div>
-</div>
+</section>
