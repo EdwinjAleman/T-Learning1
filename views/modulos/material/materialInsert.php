6bdf5ab2a22ae16800366aae442e4c1658116fee
EdwinjAleman <jeagudelo152@misena.edu.co>
2020-12-09 19:12:21 -0500

    ...

--- views/modulos/material/materialInsert.php
+++ views/modulos/material/materialInsert.php
@@ -1,27 +1,31 @@
-<h1> Subir Material De Apoyo </h1>
+<section id="main-content">
+    <section class="wrapper">
+        <h1> Subir Material De Apoyo </h1>
 
-<form name="formmaterial" id="formmaterial">
+        <form name="formmaterial" id="formmaterial">
 
-    <input type="text" name="id" hidden> <br>
+            <input type="text" name="id" hidden> <br>
 
-    <label for="titulo"> Titulo  </label> <br>
-    <input type="text" name="titulo" id="titulo"> <br>
+            <label for="titulo"> Titulo </label> <br>
+            <input type="text" name="titulo" id="titulo"> <br>
 
-    <label for="fecpub"> Fecha inicio: </label> <br>
-    <input type="date" name="fecpub" id="fecpub"> <br>
+            <label for="fecpub"> Fecha inicio: </label> <br>
+            <input type="date" name="fecpub" id="fecpub"> <br>
 
-    <label for="descrp"> Descripcion: </label> <br>
-    <textarea name="descrp" id="descrp" placeholder="Descripcion"></textarea> <br>
+            <label for="descrp"> Descripcion: </label> <br>
+            <textarea name="descrp" id="descrp" placeholder="Descripcion"></textarea> <br>
 
-    <label for="fasid"> Fase: </label> <br>
-    <input type="text" name="fasid" id="fasid"> <br>
+            <label for="fasid"> Fase: </label> <br>
+            <input type="text" name="fasid" id="fasid"> <br>
 
-    <label for="usuid"> usuario: </label> <br>
-    <input type="text" name="usuid" id="usuid"> <br>
-    
-    <label for="archurl">archivo</label><br>
-    <input lang="es" required type="file"  id="archurl"><br>
-                     
-    <input type="button" value="Crear" id="btnguardar" onclick="CrearMaterial();">
+            <label for="usuid"> usuario: </label> <br>
+            <input type="text" name="usuid" id="usuid"> <br>
 
-</form>
+            <label for="archurl">archivo</label><br>
+            <input lang="es" required type="file" id="archurl"><br>
+
+            <input type="button" value="Crear" id="btnguardar" onclick="CrearMaterial();">
+
+        </form>
+    </section>
+</section>
--- views/modulos/material/materialView.php
+++ views/modulos/material/materialView.php
@@ -1,11 +1,18 @@
-<div id="main">
- 
-	<script src="../assets/js/modulos/scripts.material.js"></script>
+<section id="main-content">
+    <section class="wrapper">
+        <div id="main">
 
-    <div id="tview">
-        <?php  include_once('materialInsert.php'); ?>
-        <?php  include_once('materialSelect.php'); ?>
-    </div>
+            <script src="../assets/js/modulos/scripts.material.js"></script>
 
-</div>
-   
+
+            <div id="inmaterial">
+                <?php  include_once('materialInsert.php'); ?>
+            </div>
+            <div id="tview">
+
+                <?php  include_once('materialSelect.php'); ?>
+            </div>
+
+        </div>
+    </section>
+</section>
