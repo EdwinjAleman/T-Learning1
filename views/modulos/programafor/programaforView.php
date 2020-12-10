1059d739338649106462d8b959bd374b0b8a1df7
EdwinjAleman <jeagudelo152@misena.edu.co>
2020-12-10 10:05:20 -0500

    ole

--- views/modulos/programafor/programaforView.php
+++ views/modulos/programafor/programaforView.php
@@ -1,6 +1,6 @@
 <section id="main-content">
     <section class="wrapper">
-    <script src="../assets/js/modulos/scripts.programafor.js"></script>
+        <script src="../assets/js/modulos/scripts.programafor.js"></script>
         <section id="main-content">
             <section class="wrapper">
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formprogramafor">Nuevo
@@ -8,14 +8,14 @@
             </section>
         </section>
 
-
-
         <div id="main">
 
-            
+            <div id="profo">
+                <?php  include_once('programaforInsert.php'); ?>
+            </div>
 
             <div id="tview">
-                <?php  include_once('programaforInsert.php'); ?>
+
                 <?php  include_once('programaforSelect.php'); ?>
             </div>
 
